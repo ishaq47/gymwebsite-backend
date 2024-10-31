69ca98ca9ed169d0824055bacefa0a38103f03f3
Muhammad Ishaq <mik9649820@gmail.com>
2024-10-31 16:42:56 +0500

    changes done

--- index.js
+++ index.js
@@ -17,7 +17,6 @@ const app = express();
 
 app.use(cors());
 app.use(express.json());
-
 app.use('/api', authRoutes);
 app.use('/api', userRoutes);
 app.use('/api/admin', adminRoutes);
--- middleware/multerMiddleware.js
+++ middleware/multerMiddleware.js
@@ -3,15 +3,17 @@ const path = require('path');
 
 // Define disk storage configuration
 const storage = multer.diskStorage({
-  destination: (req, file, cb) => {
-    const tempDir = path.resolve('/tmp');
-    cb(null, tempDir);
+  destination: function (req, file, cb) {
+    cb(null, 'uploads/');
   },
-  filename: (req, file, cb) => {
-    cb(null, `${Date.now()}-${file.originalname}`);
-  },
+
+  filename: function (req, file, cb) {
+    const uniqueSuffix = Date.now() + '-' + Math.round(Math.random() * 1E9);
+    cb(null, uniqueSuffix + '-' + file.originalname);
+  }
+
 });
 
-const uploadOnMulter = multer({ storage });
+const uploadOnMulter = multer({ storage: storage }).single('photo');
 
 module.exports = { uploadOnMulter };
