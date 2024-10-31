2c5f7d76fa7f5cadccd7e11fb7747f1ae208bee8
Muhammad Ishaq <mik9649820@gmail.com>
2024-10-31 10:18:55 +0500

    changes

--- routes/authRoutes.js
+++ routes/authRoutes.js
@@ -4,6 +4,7 @@ const { signup, login, getProfile } = require('../controllers/authController.js'
 const { protect } = require('../middleware/authMiddleware.js');
 const router = express.Router();
 
+router.get('/',(req, res) => {res.send("Welcome")})
 router.post('/signup', signup);
 router.post('/login', login);
 router.get('/profile', protect, getProfile);
